f10a0b6870e8a1b15deeed3d1dd55484b033d74e
vamc <vamc.koduri@gmail.com>
2016-12-03 00:03:03 +0530

    20 implemented update and delete

--- src/main/java/org/learning/javabrains/messenger/MessagesResources.java
+++ src/main/java/org/learning/javabrains/messenger/MessagesResources.java
@@ -3,8 +3,10 @@ package org.learning.javabrains.messenger;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,6 +16,8 @@ import org.learning.javabrains.messenger.model.Message;
 import org.learning.javabrains.messenger.service.MessengerService;
 
 @Path("messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessagesResources {
 
 	MessengerService service = new MessengerService();
@@ -26,16 +30,32 @@ public class MessagesResources {
 
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId") long id) {
 		return service.getMessage(id);
 	}
 
 	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message addMessage(Message mesg) {
 		return service.addMessage(mesg);
 
 	}
+
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long id, Message mesg) {
+		mesg.setMessageId(id);
+		return service.updateMessage(mesg);
+
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long id) {
+		
+		return service.removeMessage(id);
+
+	}
+	
+	
+	
 }
