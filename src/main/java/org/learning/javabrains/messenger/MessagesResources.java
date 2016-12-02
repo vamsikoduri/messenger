358c53285c8421167e1926d4b8da5800402121da
vamc <vamc.koduri@gmail.com>
2016-12-02 17:56:35 +0530

    13 - creating a resource

---
+++ src/main/java/org/learning/javabrains/messenger/MessagesResources.java
@@ -0,0 +1,17 @@
+package org.learning.javabrains.messenger;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("messages")
+public class MessagesResources {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMessage() {
+		return "Hello world !! ";
+	}
+
+}
